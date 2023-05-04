e76331780a00eca120881b18d4ce30cfff1c9387
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-04 23:42:15 +0300

    repository bug fixed

--- order-service/src/main/java/com/gucardev/orderservice/repository/OrderItemRepository.java
+++ order-service/src/main/java/com/gucardev/orderservice/repository/OrderItemRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<Long, OrderItem> {}
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
