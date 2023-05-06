9c29711eff69f246b49fa201a429eea7d09a5ba9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-06 14:53:24 +0300

    common service imported from product-service

--- product-service/src/main/java/com/gucardev/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/gucardev/productservice/ProductServiceApplication.java
@@ -2,12 +2,14 @@ package com.gucardev.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.gucardev.productservice", "com.gucardev.common"})
 public class ProductServiceApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(ProductServiceApplication.class, args);
   }
-
 }
