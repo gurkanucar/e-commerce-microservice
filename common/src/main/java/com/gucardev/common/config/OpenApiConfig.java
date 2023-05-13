d16a39db35feb24139c77a2f1ad3d182c66045ca
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-13 14:06:59 +0300

    initial commit

---
+++ common/src/main/java/com/gucardev/common/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.gucardev.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI(
+      @Value("${application-description}") String description,
+      @Value("${application-version}") String version) {
+    final String securitySchemeName = "bearerAuth";
+    return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+        .info(
+            new Info()
+                .title("API")
+                .version(version)
+                .description(description)
+                .license(new License().name("API Licence")));
+  }
+}
--- product-service/src/main/java/com/gucardev/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/gucardev/productservice/ProductServiceApplication.java
@@ -3,13 +3,14 @@ package com.gucardev.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan({"com.gucardev.productservice", "com.gucardev.common"})
+@PropertySource("classpath:application.properties")
 public class ProductServiceApplication {
 
   public static void main(String[] args) {
-
     SpringApplication.run(ProductServiceApplication.class, args);
   }
 }
